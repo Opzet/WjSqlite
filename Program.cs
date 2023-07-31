4416b4d6d0c0779572116faec0d383094a53d19e
David V <david@digitalgeo.com.au>
2023-07-31 22:18:30 +0800

    https://www.codeproject.com/Articles/1158937/SQLite-with-Csharp-Net-and-Entity-Framework
    Before using Entity Framework to interact with the database, you should create the SQLite database file first.
    You can use tools like "DB Browser for SQLite" or "SQLite CLI" to create an empty SQLite database file (with the .sqlite or .db extension).
    
     SQLite, it does not have built-in support for automatic database creation through Entity Framework's CreateDatabase method. Instead, you need to create the database file externally (e.g., using SQLite tools or code)
     and then let Entity Framework connect to the existing database.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,59 @@
 ﻿using System;
+using System.Data.Common;
 using System.Data.Entity; // For DbContext and related types
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Infrastructure;
 using System.Data.SQLite; // For SQLite data access
 using System.Data.SQLite.EF6;
-
+using SQLite.CodeFirst;
 using Wisej.Web;
 
 namespace WjSqlite
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		static void Main()
-		{
-
-            // Set the custom DbConfiguration before using Entity Framework.
-            DbConfiguration.SetConfiguration(new CustomDbConfiguration());
-
-            Application.MainPage = new Page1();
-		}
-
-		//
-		// You can use the entry method below
-		// to receive the parameters from the URL in the args collection.
-		//
-		//static void Main(NameValueCollection args)
-		//{
-		//}
-	}
-    public class CustomDbConfiguration : DbConfiguration
+    static class Program
     {
-        public CustomDbConfiguration()
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
         {
 
+            // Set the custom DbConfiguration before using Entity Framework.
+            DbConfiguration.SetConfiguration(new MyConfiguration());
 
-            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
-
-            //SetProviderServices("System.Data.SQLite", SQLiteProviderServices.Instance);
-            SetProviderServices("System.Data.SQLite", (System.Data.Entity.Core.Common.DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(System.Data.Entity.Core.Common.DbProviderServices)));
+            Application.MainPage = new Page1();
         }
+
+        //
+        // You can use the entry method below
+        // to receive the parameters from the URL in the args collection.
+        //
+        //static void Main(NameValueCollection args)
+        //{
+        //}
+
     }
 
+    //[DbConfigurationType(typeof(MyConfiguration))]
+    //public class Context : DbContext
+    //{
 
+    public class MyConfiguration : DbConfiguration, IDbConnectionFactory
+    {
+        public MyConfiguration()
+        {
+            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
+            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
+
+            var providerServices = (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices));
+
+            SetProviderServices("System.Data.SQLite", providerServices);
+            SetProviderServices("System.Data.SQLite.EF6", providerServices);
+
+            SetDefaultConnectionFactory(this);
+        }
+
+        public DbConnection CreateConnection(string connectionString)
+            => new SQLiteConnection(connectionString);
+    }
 }
